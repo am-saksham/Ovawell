955d4fad6640dfb93af5191a3b4e37994a26d70d
Saksham <saksham.23bce10006@vitbhopal.ac.in>
2025-02-18 09:03:00 +0530

    final changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,4 @@
 
-
-document.addEventListener("mousemove",function(dets){
-  crsr.style.left=dets.x+30+"px"
-  crsr.style.top=dets.y+"px"
-  blur.style.left=dets.x-250+"px"
-  blur.style.top=dets.y-250+"px"
-})
-var h4all=document.querySelectorAll("#nav h4")
-h4all.forEach(function(elem){
-  elem.addEventListener("mouseenter",function(){
-    crsr.style.scale=3
-    crsr.style.border="1px solid #fff"
-    crsr.style.backgroundColor="transparent"
-  })
-  
-  elem.addEventListener("mouseleave",function(){
-    crsr.style.scale=1
-    crsr.style.border="0px solid #b31ec1cb"
-    crsr.style.backgroundColor="#b31ec1cb "
-  })
-})
 gsap.to("#nav",{
   backgroundColor: "#FFBFA9",
   duration:0.5,
@@ -315,5 +294,4 @@ document.addEventListener("DOMContentLoaded", function () {
       menu.classList.toggle("active");
   });
 });
-
 
